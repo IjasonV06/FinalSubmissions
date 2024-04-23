16944c5497e0ec0486cfd7bef61c33fe7e1e04c1
IjasonV06 <161632918+IjasonV06@users.noreply.github.com>
2024-04-23 09:44:03 -0600

    Add files via upload

---
+++ Madlib(completed).py
@@ -0,0 +1,46 @@
+inputword = input("Welcome to Jason's madlib, Press enter to start!")
+
+print(inputword)
+
+adjective = input("enter an adjective:")
+
+nationality = input("enter a nationality:")
+
+name = input("enter a random name:")
+
+noun = input("enter a noun:")
+
+adjective2 = input("enter an adjective:")
+ 
+noun2 = input("enter a noun:")
+ 
+adjective3 = input("enter an adjective:")
+ 
+adjective4 = input("enter an adjective:")
+ 
+pluralnoun = input("enter any plural noun:")
+ 
+noun3 = input("enter a noun:")
+ 
+number = input("use any number:")
+ 
+shape = input("enter a shape:")
+ 
+food = input("enter a type of food:")
+ 
+food2 = input("enter another type of food:")
+ 
+number2 = input("finally, put another number.:")
+
+
+story = (f" Pizza was invented by a {adjective} {nationality}."
+         f" Chef named {name}.To make a pizza, you need"
+         f" to take a lump of {noun}, and make a thin, round"
+         f" {adjective2} {noun2}. Then you cover it with"
+         f" {adjective3} sauce, {adjective4} cheese, and fresh"
+         f" chopped {pluralnoun}. Next you have to bake it in a very"
+         f" hot {noun3}. When it is done, cut it into {number}{shape}."
+         f" Some kids like {food} pizza the best, but my favourite is the {food2} pizza."
+         f" If I could, I would eat pizza {number2} times a day!") 
+
+print(story)
